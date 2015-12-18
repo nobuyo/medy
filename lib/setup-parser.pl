f0bcad2518dfcac7c25351089dd8547dd762231f
Mako <mnfeconicu41@gmail.com>
2015-12-18 19:41:16 +0900

    create parser for parsing setup.init (finished)

--- lib/setup-parser.pl
+++ lib/setup-parser.pl
@@ -1,3 +1,5 @@
+#!/usr/bin/perl
+
 use strict;
 use warnings;
 
@@ -12,13 +14,121 @@ sub usage {
 
 usage() if ( $#ARGV == -1 );
 
+sub show_pkg_all_info {
+	my ($pkg_info) = @_;
+	print "sdesc: $pkg_info->{'sdesc'}\n";
+	print "ldesc: \n";
+	foreach (@{$pkg_info->{'ldesc'}}) {
+		print;
+	}
+	print "\n";
+	print "category: $pkg_info->{'category'}\n";
+	print "requires: $pkg_info->{'requires'}\n";
+	print "version: $pkg_info->{'version'}\n";
+	print "install: $pkg_info->{'install'}\n";
+}
+
+sub show_pkg_info {
+	my ($pkg_info, $tag) = @_;
+	unless ($tag) {
+		show_pkg_all_info(\%$pkg_info);
+	} else {
+		print "$pkg_info->{$tag}\n" or die("tag \"$tag\" is not found.");
+	}
+}
+
 my $pkg_name = $ARGV[0];
 my $tag_name = $ARGV[1];
+my $found_pkg = 0; # false
+my %pkg_info = (
+	'sdesc' => '',
+	'ldesc' => [
+		# each lines
+	],
+	'category' => '',
+	'requires' => '',
+	'version' => '',
+	'install' => ''
+);
+my $on_ldesc = 0; # false
+
+if ( !(exists $pkg_info{$tag_name}) ) {
+	print "no such a tag: $tag_name\n";
+	exit -1;
+}
+
 
 open(SETUP_INIT, "< ../setup.ini") or die("could not open file");
 
 while (<SETUP_INIT>) {
-	if (/^@ /) {
-		print;
+	if (/^@ $pkg_name$/) {
+		$found_pkg = 1; # true
+		next;
 	}
+
+	next unless ($found_pkg);
+
+	# if package is found, tries matching as follows:
+
+	# sdesc
+	if (/^sdesc: "([^\\"]++|\\.)*+"$/) {
+		$pkg_info{'sdesc'} = $1;
+		next;
+	}
+
+	# ldesc
+	if (/^ldesc: /) {
+		$on_ldesc = 1; # true
+	}
+	if (/^ldesc: "([^\\"]++|\\.)*+("?)$/) {
+		push(@{$pkg_info{'ldesc'}}, $1);
+		# when double quote is on end of line, on_ldesc is false.
+		$on_ldesc = 0 if ($2);
+		next;
+	}
+	if ($on_ldesc && /^([^\\"]++|\\.)*("?)$/) {
+		push(@{$pkg_info{'ldesc'}}, $1);
+		# when double quote is on end of line, on_ldesc is false.
+		$on_ldesc = 0 if ($2);
+		next;
+	}
+
+	# category
+	if (/^category: ([^\n]*+)$/) {
+		$pkg_info{'category'} = $1;
+		next;
+	}
+
+	# requires
+	if (/^requires: ([^\n]*+)$/) {
+		$pkg_info{'requires'} = $1;
+		next;
+	}
+
+	# version
+	if (/^version: ([^\n]*+)$/) {
+		$pkg_info{'version'} = $1;
+		next;
+	}
+
+	# install
+	if (/^install: ([^\n]*+)$/) {
+		$pkg_info{'install'} = $1;
+		next;
+	}
+
+	# ignore [prev] info
+	last if (/^\[prev\]$/);
+	# next package
+	last if (/^@ /);
 }
+
+unless ($found_pkg) {
+	exit -1;
+}
+
+
+show_pkg_info(\%pkg_info, "$tag_name");
+
+
+
