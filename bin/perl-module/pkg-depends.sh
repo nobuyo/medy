7745975032064809ccc57bbd717dab507d8969c3
Mako <mnfeconicu41@gmail.com>
2015-12-21 22:31:59 +0900

    create perl-module (automatically generated from lib/Makefile)

---
+++ bin/perl-module/pkg-depends.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+function pkg-depends {
+perl -e '
+#!/usr/bin/perl
+# 
+#   pkg-depends.pl -- show the depended packages of given package name
+# 
+# args:
+#   $package_name <- $ARGV[0]
+# 
+# input:
+#   if a enviroment variable "SETUP_INI_FILE_PATH" is exported, input from its file.
+#   otherwise, input from stdin.
+#   how output the nested all requirements of Vim is as follows:
+# 
+#       cat /path/to/setup.ini | perl pkg-depends.pl vim
+#   or
+#       export SETUP_INI_FILE_PATH=/path/to/setup.ini
+#       perl pkg-depends.pl vim
+# 
+# return:
+#   print the depended packages of given package name.
+#   return -1 if package name is not found in file $ENV{"SETUP_INI_FILE_PATH"}.
+# 
+
+use strict;
+use warnings;
+use File::Basename;
+
+package PkgDepends;
+
+sub usage {
+	my $script_name = $0;
+	print "Usage\n";
+	print "  \"perl $script_name <package>\"   to show dependencies of package\n";
+	exit;
+}
+
+# create requirements table
+# return the structure like:
+# 
+#     (
+#         "4ti2-debuginfo" => ["cygwin-debuginfo"],
+#         "a2ps"           => ["bash", "libiconv2", "libintl8", "libpaper1", ..., "cygwin"],
+#           :
+#         "zsh"            => ["cygwin", "libncursesw10", "libpcre1", "libiconv2", "libgdbm4", "_update-info-dir"]
+#     )
+# 
+sub create_requirements_table {
+	my %pkg_requirements_table = ();
+
+	# select input (file or stdin)
+	my $in;
+	my $target_file = $ENV{"SETUP_INI_FILE_PATH"};
+	if ($target_file) {
+		open($in, "< $target_file") or die("could not open file \"$target_file\"");
+	} else {
+		$in = *STDIN
+	}
+
+	my $pkg_name;
+	my @pkg_requires;
+	while (<$in>) {
+		if (/^@ (.*)$/) {
+			$pkg_name = $1;
+		}
+
+		if (/^requires: (.*)$/) {
+			@pkg_requires = split(/\s+/, $1);
+			@{$pkg_requirements_table{$pkg_name}} = @pkg_requires;
+		}
+	}
+	\%pkg_requirements_table;
+}
+
+# fetch the package all nested dependency
+# 
+# args:
+#   $pkg_requirements_table - requirements table, see create_requirements_table().
+#   $require_pkgs           - empty hash, this function stores the required packages to the hash.
+#   $pkg_name               - package name, to fetch its dependent packages.
+#   $nest                   - nest level, starts with 0
+# 
+# return:
+#   nothing
+# 
+sub fetch_pkg_depends {
+	my ($pkg_requirements_table, $require_pkgs, $pkg_name, $nest) = @_;
+	my @requires = @{ $pkg_requirements_table->{$pkg_name} || [] };
+
+	# print "begin: $nest\n";
+	# print join(" ", keys %require_pkgs) . "\n";
+
+	foreach (@requires) {
+		my $require_pkg = $_;
+		my $marked_pkg  = \$require_pkgs->{$_};
+		next if (defined $$marked_pkg && $$marked_pkg == 1); # already marked
+		$$marked_pkg = 1; # mark
+		fetch_pkg_depends(\%$pkg_requirements_table, \%$require_pkgs, $require_pkg, $nest + 1); # recursion
+	}
+
+	# print "end: $nest\n";
+}
+
+if (__FILE__ eq $0) {
+	usage() if ($#ARGV == -1);
+	my $pkg_name = $ARGV[0];
+	my %pkg_requirements_table = %{ create_requirements_table() };
+	my %require_pkgs = ();
+
+	fetch_pkg_depends(\%pkg_requirements_table, \%require_pkgs, $pkg_name, 0);
+
+	# print "---\n";
+	print join("\n", keys %require_pkgs) . "\n";
+} else {
+	1;
+}
+
+
+
+' -- $@
+}
---
+++ bin/perl-module/setup-parser.sh
@@ -0,0 +1,186 @@
+#!/usr/bin/env bash
+
+function setup-parser {
+perl -e '
+#!/usr/bin/perl
+# 
+#   setup-parser.pl -- perser to read setup.int
+# 
+# args:
+#   $package_name <- $ARGV[0]
+#   $tag_name     <- $ARGV[1]
+#     where $tag_name = "sdesc", "ldesc", "category", "requires", "version", "install", undef
+# 
+# input:
+#   if a enviroment variable "SETUP_INI_FILE_PATH" is exported, input from its file.
+#   otherwise, input from stdin.
+#   how output the requirements of Vim is as follows:
+#   
+#       cat /path/to/setup.ini | perl setup-parser.pl vim requires
+#   or
+#       export SETUP_INI_FILE_PATH=/path/to/setup.ini
+#       perl setup-parser.pl vim requires
+# 
+# return:
+#   return -1 if package name is not found in input.
+#   return -1 if tag name is invalid.
+#   if $tag_name is not specified, print $package_name"s info.
+#   otherwise, print the tagged content of $package_name.
+# 
+
+use strict;
+use warnings;
+
+package SetupParser;
+
+sub usage {
+	my $script_name = $0;
+	print "$script_name: perser to read setup.int\n";
+	print "Usage:\n";
+	print "  perl $script_name <package>         to show package info\n";
+	print "  perl $script_name <package> <tag>   to show tagged content on package info\n";
+	print "<tag>:\n";
+	print "  sdesc  ldesc  category  requires  version  install\n";
+	exit;
+}
+
+sub format_pkg_all_info {
+	my ($pkg_info) = @_;
+	my @each_lines = (
+		"sdesc: $pkg_info->{"sdesc"}",
+		"ldesc: \n" . join("", @{$pkg_info->{"ldesc"}}),
+		"category: $pkg_info->{"category"}",
+		"requires: $pkg_info->{"requires"}",
+		"version: $pkg_info->{"version"}",
+		"install: $pkg_info->{"install"}",
+		"" # ends with \n
+	);
+	return join("\n", @each_lines);
+}
+
+sub format_pkg_info {
+	my ($pkg_info, $tag) = @_;
+	
+	unless (defined $tag) {
+		return format_pkg_all_info(\%$pkg_info);
+	}
+
+	if (ref $pkg_info->{$tag} eq "ARRAY") {
+		return join("", @{$pkg_info->{$tag}})
+	} else {
+		return "$pkg_info->{$tag}\n";
+	}
+}
+
+# check if the tag name is valid
+sub validate_tag_name {
+	$_[0] =~ /^(?:requires|sdesc|ldesc|category|version|install)$/;
+}
+
+sub extract_from_setup_init {
+	my ($pkg_name) = @_;
+	# use for loop
+	my $found_pkg = 0; # false
+	my %pkg_info = (
+		"sdesc" => "",
+		"ldesc" => [
+			# each lines
+		],
+		"category" => "",
+		"requires" => "",
+		"version" => "",
+		"install" => ""
+	);
+	# use for extracting ldesc
+	my $on_ldesc = 0; # false
+
+	# select input (file or stdin)
+	my $in;
+	my $target_file = $ENV{"SETUP_INI_FILE_PATH"};
+	if ($target_file) {
+		open($in, "< $target_file") or die("could not open file \"$target_file\"");
+	} else {
+		$in = *STDIN
+	}
+
+	while (<$in>) {
+		# find package name
+		if (/^@ \Q$pkg_name\E$/) {
+			$found_pkg = 1; # true
+			next;
+		}
+
+		next unless ($found_pkg);
+
+		# if package is found, tries matching as follows:
+
+		# sdesc
+		if (/^sdesc: "([^"]*+)"$/) {
+			$pkg_info{"sdesc"} = $1;
+			next;
+		}
+
+		# ldesc
+		if (/^ldesc: "/) {
+			$on_ldesc = 1; # true
+		}
+		if (/^ldesc: "([^"]*+)("?)$/) {
+			push(@{$pkg_info{"ldesc"}}, $1);
+			# when double quote is on end of line, on_ldesc is false.
+			$on_ldesc = 0 if ($2);
+			next;
+		}
+		if ($on_ldesc && /^([^"]*+)("?)$/) {
+			push(@{$pkg_info{"ldesc"}}, $1);
+			# when double quote is on end of line, on_ldesc is false.
+			$on_ldesc = 0 if ($2);
+			next;
+		}
+
+		# category, requires, version, install
+		if (/^(category|requires|version|install): ([^\n]*+)$/) {
+			$pkg_info{$1} = $2;
+			next;
+		}
+
+		# ignore [prev] info
+		last if (/^\[prev\]$/);
+		# ignore next package
+		last if (/^@ /);
+	}
+
+	unless ($found_pkg) {
+		print STDERR "no such a package: $pkg_name\n";
+		exit -1;
+	}
+
+	\%pkg_info;
+}
+
+sub extract {
+	extract_from_setup_init @_;
+}
+
+
+if (__FILE__ eq $0) {
+	usage() if ( $#ARGV == -1 );
+	my $pkg_name = $ARGV[0];
+	my $tag_name = $ARGV[1];
+	my $pkg_info = extract_from_setup_init($pkg_name);
+
+	if (defined $tag_name && !validate_tag_name($tag_name)) {
+		print STDERR "no such a tag: $tag_name\n";
+		exit -1;
+	}
+
+	if (defined $tag_name) {
+		print format_pkg_info(\%$pkg_info, "$tag_name");
+	} else {
+		print format_pkg_info(\%$pkg_info);
+	}
+} else {
+	1;
+}
+
+' -- $@
+}
