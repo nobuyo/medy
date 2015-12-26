89a8df43749cb53c548f58d22ab7ddb865a99601
nobuyo <world_fantasy_2017@live.jp>
2015-12-26 20:26:54 +0900

    update medy-update()

--- bin/medy-update.sh
+++ bin/medy-update.sh
@@ -2,8 +2,20 @@
 
 function medy-update {
   setlab
-  local previous="$(grep "^@" $cache/$dir/$arch/setup.ini-save | tr -d '@')"
+  grep "^@" $cache/$dir/$arch/setup.ini-save > /tmp/medylist-save
   getsetup
-  local current="$(grep "^@" $cache/$dir/$arch/setup.ini | tr -d '@')"
-  diff "$previous" "$current"
+  grep "^@" $cache/$dir/$arch/setup.ini | tr -d '@' > /tmp/medylist
+  local dif="$(diff -u /tmp/medylist-save /tmp/medylist)"
+
+  if [ ! -z "$dif" ]; then
+    echo -e "\033[33m==>New package\033[m"
+    echo "$dif" | grep '^\+\s' | sed -e 's/\+ //g'
+
+    echo -e "\033[33m==>Deleted package\033[m"
+    echo "$dif"| grep '^-\s' | sed -e 's/- //g'
+  else
+    echo "setup.ini already latest version"
+  fi
+
+  # rm /tmp/medylist /tmp/medylist-save
 }
