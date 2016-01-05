fae24a583132bf314f009ca4b25dd139982dd636
nobuyo <world_fantasy_2017@live.jp>
2016-01-05 16:22:46 +0900

    fix mirror server url

--- bin/medy-update.sh
+++ bin/medy-update.sh
@@ -2,18 +2,18 @@
 
 function medy-update {
   setlab
-  grep "^@" $cache/$dir/$arch/setup.ini-save | tr -d '@' > /tmp/medylist-save
+  # grep "^@" $cache/$dir/$arch/setup.ini-save | tr -d '@' > /tmp/medylist-save
   getsetup
-  grep "^@" $cache/$dir/$arch/setup.ini | tr -d '@' > /tmp/medylist
-  local dif="$(diff -u /tmp/medylist-save /tmp/medylist)"
-
-  if [ ! -z "$dif" ]; then
-    # if there were some changes => disp pkg name
-    echo -e "\033[33m==>Updated package\033[m"
-    echo "$dif" | grep '^\+\s' | sed -e 's/\+ //g'
-  else
-    echo "setup.ini already latest version"
-  fi
+  # grep "^@" $cache/$dir/$arch/setup.ini | tr -d '@' > /tmp/medylist
+  # local dif="$(diff -u /tmp/medylist-save /tmp/medylist)"
+  #
+  # if [ ! -z "$dif" ]; then
+  #   # if there were some changes => disp pkg name
+  #   echo -e "\033[33m==>Updated package\033[m"
+  #   echo "$dif" | grep '^\+\s' | sed -e 's/\+ //g'
+  # else
+  #   echo "setup.ini already latest version"
+  # fi
 
   # rm /tmp/medylist /tmp/medylist-save
 }
