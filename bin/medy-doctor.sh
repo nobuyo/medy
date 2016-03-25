dbce632c141c7882ed360e7c38b1be214729c757
nobuyo <world_fantasy_2017@live.jp>
2016-03-25 22:02:21 +0900

    wip:doctor checking package

--- bin/medy-doctor.sh
+++ bin/medy-doctor.sh
@@ -7,20 +7,31 @@ function medy-doctor {
   local pkg
   local ready=1
 
-  echo "Checking...."
+  echo -n "Checking packages availablity.... "
 
   for pkg in $installed;
   do
-  	is-available $pkg ||
-    medy-info $pkg &>/dev/null  || {
-      echo "$pkg is not available"
-      ready=0
+    is-available $pkg || { #medy-info $pkg &>/dev/null || {
+        echo; echo "$pkg is not available"
+        ready=0
     }
   done
 
   if [ "$ready" = 1 ]; then
-    echo "Your system is healty, ready to medy"
-  else
-    :
+    echo -e "\033[32mOK!\033[m"
+  fi
+
+    echo -n "Checking update.... "
+
+  wget -q https://raw.githubusercontent.com/nobuyo/medy/master/medy -O - |\
+   ( cat $medy | diff /dev/fd/3 -) 3<&0 &>/dev/null ||
+  {
+    echo;
+    warn ":medy has update, please run \"medy upgrade-self\""
+    ready=0
+  }
+
+  if [ "$ready" = 1 ]; then
+    echo -e "\033[32mOK!\033[m"
   fi
 }
